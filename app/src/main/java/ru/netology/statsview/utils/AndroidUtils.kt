8ee07670d42c2e830cd23fcf72d40b2572aebcb6
Alesya Hitrova <alesyahitrova@yandex.ru>
2023-10-15 00:12:26 +0300

    Initial commit

---
+++ app/src/main/java/ru/netology/statsview/utils/AndroidUtils.kt
@@ -0,0 +1,12 @@
+package ru.netology.statsview.utils
+
+import android.content.Context
+import kotlin.math.ceil
+
+
+object AndroidUtils {
+
+    fun dp(context: Context, dp:Int):Int =
+        ceil(context.resources.displayMetrics.density*dp).toInt()
+
+}
